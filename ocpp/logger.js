17096072c01ece87271db32e5446da5d5a38daa1
yanis.amraoui <yanis.amraoui@gireve.com>
2022-11-29 00:10:42 +0100

    second commit

---
+++ ocpp/logger.js
@@ -0,0 +1,71 @@
+import fs from 'fs';
+import debugFn from 'debug';
+import OCPPCommands from './commands';
+
+import {
+  DEBUG_LIBNAME
+} from './constants';
+import Websocket from 'ws';
+
+const debug = debugFn(DEBUG_LIBNAME);
+
+export const LOGGER_URL = '/logger';
+
+const COUNT_MESSAGE_TO_STORE = 100;
+
+export default class Logger {
+  constructor(centralSystem) {
+    this.centralSystem = centralSystem;
+    this.sockets = [];
+    this.messages = [];
+  }
+
+  addSocket(socket) {
+    this.sockets.push(socket);
+
+    socket.on('message', async (message) => {
+      const [requestedMessage, payload] = JSON.parse(message);
+
+      for (let client of this.centralSystem.clients) {
+        if (requestedMessage === 'StartTransaction') {
+          await client.connection.send(new OCPPCommands.RemoteStartTransaction(payload));
+        } else if (requestedMessage === 'StopTransaction') {
+          await client.connection.send(new OCPPCommands.RemoteStopTransaction(payload));
+        } else {
+          if (!payload.connectorId) {
+            delete payload.connectorId;
+          }
+          await client.connection.send(new OCPPCommands.TriggerMessage({ requestedMessage, ...payload }));
+        }
+      }
+    });
+
+    socket.send(JSON.stringify({ command: 'history', messages: this.messages }));
+  }
+
+  debug(message) {
+    if (typeof message !== 'string') {
+      message = JSON.stringify(message);
+    }
+    this.messages.push(message);
+    if (this.messages.length > COUNT_MESSAGE_TO_STORE) {
+      this.messages = this.messages.slice(this.messages.length - COUNT_MESSAGE_TO_STORE, this.messages.length)
+    }
+
+    debug(message);
+    fs.appendFile(__dirname + '../messages.log', message, function() {});
+    this.sendMessage({ command: 'message', message });
+  }
+
+  sendMessage(messageToSend) {
+    if (typeof messageToSend !== 'string') {
+      messageToSend = JSON.stringify(messageToSend);
+    }
+
+    for (let socket of this.sockets) {
+      if (socket.readyState === Websocket.OPEN) {
+        socket.send(messageToSend);
+      }
+    }
+  }
+}
---
+++ pages/index.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="container p-lg-top-bottom">
+
+    <v-btn @click="send('BootNotification')">BootNotification</v-btn>
+    <v-btn @click="send('DiagnosticsStatusNotification')">DiagnosticsStatusNotification</v-btn>
+    <v-btn @click="send('FirmwareStatusNotification')">FirmwareStatusNotification</v-btn>
+    <v-btn @click="send('Heartbeat')">Heartbeat</v-btn>
+    <v-btn @click="send('StatusNotification')">StatusNotification</v-btn>
+
+    <v-divider></v-divider>
+    <br/>
+
+    <v-flex xs12 sm6>
+      <v-text-field
+          label="ConnectionID"
+          single-line v-model="connectorId"
+      ></v-text-field>
+    </v-flex>
+    <v-flex xs12 sm6>
+      <v-text-field
+          label="IDTag"
+          single-line v-model="idTag"
+      ></v-text-field>
+    </v-flex>
+
+    <v-btn @click="send('MeterValues')">MeterValues</v-btn>
+    <v-btn @click="send('StartTransaction')">Start transaction</v-btn>
+    <v-btn @click="send('StopTransaction')">Stop transaction</v-btn>
+
+    <v-divider></v-divider>
+    <br/>
+    Log:
+    <log-list :list="logMessages"></log-list>
+
+  </div>
+</template>
+<style>
+  .log-message {
+    font-family: "Courier New", Courier, "Lucida Sans Typewriter", "Lucida Typewriter", monospace;
+    padding: 5px 0;
+    border-bottom: 1px dashed #AAA;
+  }
+</style>
+<script>
+  import Vue from 'vue';
+  import Component, { namespace } from 'nuxt-class-component';
+  import LogList from '~/components/LogList';
+
+  const { State, Mutation, Action } = namespace('socket');
+
+  export default @Component({
+    components: {
+      LogList
+    },
+    head: {
+      title: 'Welcome'
+    }
+  })
+  class IndexPage extends Vue {
+    @State logMessages;
+
+    connectorId = '';
+    idTag = '';
+
+    mounted () {
+    }
+
+    send(command) {
+      this.$socket.send(JSON.stringify([command, { connectorId: +this.connectorId, idTag: this.idTag}]))
+    }
+  }
+</script>
